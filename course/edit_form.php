b907d100b1e167f3288347de098d65d678b933af
denismoraes <denis_rogerio.moraes@outlook.com>
2019-03-29 14:00:18 -0300

    Update edit_form.php

--- course/edit_form.php
+++ course/edit_form.php
@@ -55,7 +55,7 @@ class course_edit_form extends moodleform {
         $mform->setType('returnurl', PARAM_LOCALURL);
         $mform->setConstant('returnurl', $returnurl);
 
-        $mform->addElement('text','fullname', get_string('fullnamecourse'),'maxlength="254" size="50"');
+        $mform->addElement('text','fullname', get_string('fullnamecourse'),'maxlength="255" size="50"');
         $mform->addHelpButton('fullname', 'fullnamecourse');
         $mform->addRule('fullname', get_string('missingfullname'), 'required', null, 'client');
         $mform->setType('fullname', PARAM_TEXT);
